e852b965397f2df3d5d93dec9c31460893f55c86
SebastianVillarreal <sebas_villalva13@hotmail.com>
2021-07-14 21:10:16 -0500

    commit inicial

---
+++ LMApp/Shared/LMAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMAppApp.swift
+//  Shared
+//
+//  Created by Sebastian Villarreal on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LMAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
